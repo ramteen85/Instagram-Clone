e4a877eb66c24fdd2a25e7d69534c0c97c244d25
Ramteen Taheri <ramteen.taheri@gmail.com>
2020-04-24 14:40:40 +1000

    Initial Commit

--- app/src/main/java/com/cliquenextwebsolutions/instagramclone/SignUp.java
+++ app/src/main/java/com/cliquenextwebsolutions/instagramclone/SignUp.java
@@ -6,15 +6,26 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import com.parse.FindCallback;
+import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.SaveCallback;
 import com.shashank.sony.fancytoastlib.FancyToast;
+
+import java.util.List;
 
 public class SignUp extends AppCompatActivity implements View.OnClickListener {
 
     private Button btnSave;
     private EditText edtName, edtPunchSpeed, edtPunchPower, edtKickSpeed, edtKickPower;
+    private TextView txtGetData;
+    private Button btnGetAllData;
+    private Button btnTransition;
+    private String allKickboxers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +40,60 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
         edtPunchPower = findViewById(R.id.edtPunchPower);
         edtKickSpeed = findViewById(R.id.edtKickSpeed);
         edtKickPower = findViewById(R.id.edtKickPower);
+
+        txtGetData = findViewById(R.id.txtGetData);
+
+        btnGetAllData = findViewById(R.id.btnGetAllData);
+
+        btnTransition = findViewById(R.id.btnNextActivity);
+
+        txtGetData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseQuery<ParseObject> parseQuery = ParseQuery.getQuery("Kickboxer");
+                parseQuery.getInBackground("QZ43EN6vGE", new GetCallback<ParseObject>() {
+                    @Override
+                    public void done(ParseObject object, ParseException e) {
+                        if(object != null && e == null) {
+                            txtGetData.setText(object.get("name") + "");
+                        }
+                    }
+                });
+            }
+        });
+
+        btnGetAllData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                allKickboxers = "";
+                ParseQuery<ParseObject> queryAll = ParseQuery.getQuery("Kickboxer");
+                queryAll.whereGreaterThan("punchPower", 100);
+                queryAll.findInBackground(new FindCallback<ParseObject>() {
+                    @Override
+                    public void done(List<ParseObject> objects, ParseException e) {
+                        if(e == null) {
+                            if(objects.size() > 0) {
+                                for (ParseObject kickboxer : objects) {
+                                    allKickboxers = allKickboxers + kickboxer.get("name") + "\n";
+                                }
+
+                                FancyToast.makeText(SignUp.this, allKickboxers, FancyToast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
+                            }
+                        } else {
+                            FancyToast.makeText(SignUp.this,  "Error: Could not fetch data...", FancyToast.LENGTH_SHORT, FancyToast.ERROR, true).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        btnTransition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
     }
 
---
+++ app/src/main/java/com/cliquenextwebsolutions/instagramclone/SignUpLoginActivity.java
@@ -0,0 +1,4 @@
+package com.cliquenextwebsolutions.instagramclone;
+
+public class SignUpLoginActivity {
+}
